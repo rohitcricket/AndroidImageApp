cff96d44fec49e16358be7995bda2f1d89459206
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro-2.local>
2016-09-28 20:55:58 -0700

    first commit; added libraries - glide, retrofit and gsun.

---
+++ app/src/main/java/com/example/android/imagineering/FlickrAPI.java
@@ -0,0 +1,10 @@
+package com.example.android.imagineering;
+
+/**
+ * Created by rohitgupta on 9/28/16.
+ */
+public interface FlickrAPI {
+
+    String FlickrApiKey = "";
+    String FlickrApiSecret = "";
+}
