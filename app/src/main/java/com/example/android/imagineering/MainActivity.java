e6b2786c0cbe6691ee7c1a36f9ac28078f8b2781
ROHIT GUPTA <rohitgupta@ROHITs-MacBook-Pro-2.local>
2016-10-03 19:58:25 -0700

    implemented Glide

--- app/src/main/java/com/example/android/imagineering/MainActivity.java
+++ app/src/main/java/com/example/android/imagineering/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.imagineering;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+//Initialize ImageView
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+//Loading image from below url into imageView
+        Glide.with(this)
+                .load("http://wallpaperrs.com/uploads/cars/bmw-7-series-3725.jpg")
+                .into(imageView);
     }
 }
